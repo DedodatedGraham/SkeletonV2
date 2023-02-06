147408b778a78c3e634391b70cc9a5ee87c4e50f
Graham_Garcia1 <grahamg7584@gmail.com>
2023-02-06 16:40:45 -0600

    quickcommit

--- runs/basiliskRuns/clean.sh
+++ runs/basiliskRuns/clean.sh
@@ -1,6 +1,6 @@
 rm -r dump* 2> /dev/null
 rm -r snapshot* 2> /dev/null
-rm -r SkeletonV2/ 2> /dev/null
+rm -r ../src 2> /dev/null
 rm -r vof* 2> /dev/null
 rm -r skeleton-* 2> /dev/null
 rm -r .qcc* 2> /dev/null
