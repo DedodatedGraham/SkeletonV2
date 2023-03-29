8ee1b941c0e5e026c49f3789045e9d862d1c18ec
Graham_Garcia1 <grahamg7584@gmail.com>
2023-03-29 16:44:04 -0500

    quickcommit

--- runs/basiliskRuns/clean.sh
+++ runs/basiliskRuns/clean.sh
@@ -7,5 +7,7 @@ rm -r .qcc* 2> /dev/null
 rm -r infc* 2> /dev/null
 rm -r mtrace* 2> /dev/null
 rm -r intsmooth* 2> /dev/null
+rm -r smoothskeleton-* 2> /dev/null
+rm -r intdata-* 2> /dev/null
 rm -r orderedinterface* 2> /dev/null
 rm drop 2> /dev/null
