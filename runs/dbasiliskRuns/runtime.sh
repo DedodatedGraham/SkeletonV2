6191448a8ed365d6ce9219e3b4b5a301f832b68c
Graham_Garcia1 <grahamg7584@gmail.com>
2023-08-10 13:00:26 -0500

    quickcommit

--- runs/dbasiliskRuns/runtime.sh
+++ runs/dbasiliskRuns/runtime.sh
@@ -21,7 +21,7 @@ fi
 ./compile.sh
 #valgrind ./drop $time_start $time_end
 ./drop $time_start $time_end
-#./../pScript/run.sh $time_start $time_end
+./../pScript/run.sh $time_start $time_end
 end=`date +%s.%N`
 runtime=$( echo "$end - $start" | bc -l )
 echo $runtime
